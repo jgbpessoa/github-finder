3710d547e3043d9f0fa37c38065d309d795460e6
jgbpessoa <jgbpessoa@gmail.com>
2022-04-19 11:27:23 -0500

    Setup GithubContext

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { GithubProvider } from "./context/github/GithubContext";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import About from "./pages/About";
@@ -7,19 +8,21 @@ import Footer from "./components/layout/Footer";
 
 function App() {
   return (
-    <Router>
-      <main className="flex flex-col justify-between h-screen">
-        <Navbar />
-        <main className="container mx-auto px-3 pb-12">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/*" element={<NotFound />} />
-          </Routes>
+    <GithubProvider>
+      <Router>
+        <main className="flex flex-col justify-between h-screen">
+          <Navbar />
+          <main className="container mx-auto px-3 pb-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/*" element={<NotFound />} />
+            </Routes>
+          </main>
+          <Footer />
         </main>
-        <Footer />
-      </main>
-    </Router>
+      </Router>
+    </GithubProvider>
   );
 }
 
