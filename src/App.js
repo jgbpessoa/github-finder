e4466a62c7cbf51301da7ea1ee3c13ad9985c088
jgbpessoa <jgbpessoa@gmail.com>
2022-04-22 12:07:19 -0500

    Created user page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,41 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { GithubProvider } from "./context/github/GithubContext";
+import { AlertProvider } from "./context/alert/AlertContext";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import About from "./pages/About";
 import Navbar from "./components/layout/Navbar";
 import Footer from "./components/layout/Footer";
+import Alert from "./components/layout/Alert";
+import User from "./pages/User";
 
 function App() {
   return (
     <GithubProvider>
-      <Router>
-        <main className="flex flex-col justify-between h-screen">
-          <Navbar />
-          <main className="container mx-auto px-3 pb-12">
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/*" element={<NotFound />} />
-            </Routes>
+      <AlertProvider>
+        <Router>
+          <main className="flex flex-col justify-between h-screen">
+            <Navbar />
+            <main className="container mx-auto px-3 pb-12">
+              <Routes>
+                <Route
+                  path="/"
+                  element={
+                    <>
+                      <Alert />
+                      <Home />
+                    </>
+                  }
+                />
+                <Route path="/about" element={<About />} />
+                <Route path="/user/:login" element={<User />} />
+                <Route path="/*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
           </main>
-          <Footer />
-        </main>
-      </Router>
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,6 +9,8 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({ children }) => {
   const initalState = {
     users: [],
+    user: {},
+    repos: [],
     loading: false,
   };
 
@@ -24,7 +26,6 @@ export const GithubProvider = ({ children }) => {
       type: "CLEAR_USERS",
     });
 
-  // Get initial users (testing purposes)
   const searchUsers = async (text) => {
     setLoading();
 
@@ -46,13 +47,64 @@ export const GithubProvider = ({ children }) => {
     });
   };
 
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
+
+    if (response.status === 404) {
+      // window.location = "/notfound";
+      console.log(response);
+    } else {
+      const data = await response.json();
+
+      dispatch({
+        type: "GET_USER",
+        payload: data,
+      });
+    }
+  };
+
+  const getUserRepos = async (login) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    });
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`,
+        },
+      }
+    );
+
+    const data = await response.json();
+
+    dispatch({
+      type: "GET_REPOS",
+      payload: data,
+    });
+  };
+
   return (
     <GithubContext.Provider
       value={{
         users: state.users,
         loading: state.loading,
+        user: state.user,
+        repos: state.repos,
         searchUsers,
         clearUsers,
+        getUser,
+        getUserRepos,
       }}
     >
       {children}
